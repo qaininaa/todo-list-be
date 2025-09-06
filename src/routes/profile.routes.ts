8c4d8bddf7d1a70f60441d71afb7749bc37a826b
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-09-06 12:33:42 +0700

    Refactor: add type annotation for router in auth and profile routes

--- src/routes/profile.routes.ts
+++ src/routes/profile.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import * as profileController from "../controllers/profile.controller";
 
-const router = express.Router();
+const router: express.Router = express.Router();
 
 router.get("/user", profileController.profileUser);
 
